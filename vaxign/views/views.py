3425d4d71622e1a3f847535a7f92b99deb414dfb
Unknown <e4ong1031@gmail.com>
2020-02-04 11:27:33 -0500

    Fixed login not correctly retrieve session info.

--- vaxign/views/views.py
+++ vaxign/views/views.py
@@ -26,6 +26,17 @@ logger = logging.getLogger('console')
 
 def index(request, name):
     
+    try:
+        php_session_str = open(str.format('{}/sess_{}', settings.SESSION_FILE_PATH, request.COOKIES.get('PHPSESSID'))).read().encode()
+        php_session = phpserialize.loads(php_session_str)
+        request.session['c_user_name'] = php_session['c_user_name'.encode()].decode()
+        request.session['is_admin'] = php_session['is_admin'.encode()]
+    except:
+        if 'c_user_name' in request.session.keys():
+            del request.session['c_user_name']
+        if 'is_admin' in request.session.keys():
+            del request.session['is_admin']
+    
     if name == 'index' or name not in ['dynamic', 'precompute']:
         name = 'dynamic'
     context = {'vaxign_option': name}
@@ -227,13 +238,13 @@ def login(request):
         request.session['c_user_name'] = php_session['c_user_name'.encode()].decode()
         request.session['is_admin'] = php_session['is_admin'.encode()]
     except:
-        del request.session['c_user_name']
-        del request.session['is_admin']
+        if 'c_user_name' in request.session.keys():
+            del request.session['c_user_name']
+        if 'is_admin' in request.session.keys():
+            del request.session['is_admin']
     
     return redirect('/vaxign2')
     
-    return redirect('/vaxign2')
-
 def logout(request):
     
     del request.session['c_user_name']
